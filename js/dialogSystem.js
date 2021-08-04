d2ef9f53e5bcdd5c64b374c0c2850094c69099a5
Tony <tony@aweapp.com>
2021-08-04 22:57:36 +0800

    dialogSystem Review#1

--- js/dialogSystem.js
+++ js/dialogSystem.js
@@ -1,10 +1,15 @@
-let index = 0;
-let data;
 function DialogSystem() {
 
+    let self = this;
+
+    this.index = 0;
+    this.data;
+    this.button;
+    this.dialogFont;
+
     this.onDraw = function () {
-        if (data.text.length > index) {
-            button.style('display', 'auto');
+        if ( self.data.text.length > self.index) {
+            self.button.style('display', 'auto');
             fill(0, 0, 0, 150);
             rect(0, 0, windowWidth, windowHeight);
             fill(69, 123, 157);
@@ -12,26 +17,34 @@ function DialogSystem() {
             fill(255);
             textSize(36);
             textAlign(LEFT);
-            textFont(dialogFont);
-            text(data.text[index], 20, windowHeight - windowHeight / 4.5, windowWidth, windowHeight / 7);
-            button.mousePressed(nextDialog);
+            textFont(self.dialogFont);
+            text(self.data.text[self.index], 20, windowHeight - windowHeight / 4.5, windowWidth, windowHeight / 7);
+            self.button.mousePressed( self.nextDialog );
         } else {
-            button.style('display', 'none');
+            self.button.style('display', 'none');
         }
     }
 
     // init setup
     this.init = function (jsonFile) {
-        data = jsonFile;
+        self.data = jsonFile;
+
+        self.button = createButton('NEXT');
+        self.button.position(windowWidth - 150, windowHeight - 90);
+        self.button.size(100, 50);
+        self.button.id('dialogButton');        
+	    self.dialogFont = loadFont("assets/fonts/CabinSketch-Regular.ttf");
     }
 
-}
+    this.nextDialog = function() {
 
-function nextDialog() {
-
-    if (data.text.length > index) {
-        index += 1;
-        console.log(index);
+        if (self.data.text.length > self.index) {
+            self.index += 1;
+            console.log(self.index);
+        }
+    
     }
 
-}
+    
+};
+
--- js/mainLoop.js
+++ js/mainLoop.js
@@ -3,9 +3,8 @@ let gStageManager = null;
 let gPlayerManager = null;
 let gSpriteManager = null;
 let gInputManager = null;
-let dialogJSON = null;
-let dialogFont = null;
-let button;
+
+
 function preload() {
 
 	gSceneManager = new SceneManager()
@@ -14,22 +13,18 @@ function preload() {
 	gPlayerManager = new PlayerManager();
 	gInputManager = new InputManager();
 	gDialogSystem = new DialogSystem();
-	gSpriteManager.preload();
-
-	dialogJSON = loadJSON('test.json');
-	dialogFont = loadFont("..\\assets\\fonts\\CabinSketch-Regular.ttf");
-
-	button = createButton('NEXT');
-	button.position(windowWidth - 150, windowHeight - 90);
-	button.size(100, 50);
-	button.id('dialogButton');
-
-
 	gUIManager = new UIManager();
 
 	gUIManager.preload();
 	gSpriteManager.preload();
 	gPlayerManager.preload();
+    
+	let dialogJSON = loadJSON('test.json');
+
+
+	gDialogSystem.init(dialogJSON);
+
+
 }
 
 function setup() {
@@ -45,11 +40,8 @@ function draw() {
 	gInputManager.onUpdate();
 	gStageManager.onDraw();
 
-	drawSprites();
 
-	gDialogSystem.init(dialogJSON);
 	gDialogSystem.onDraw();
-
 
 
 	gPlayerManager.onDraw();
